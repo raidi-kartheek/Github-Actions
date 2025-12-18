b147dd4aaf825cda71b75b2320ed2db3e21d4de0
Your Name <you@example.com>
2025-12-18 13:31:16 +0530

    msg

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "raidi.fun"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z06311461V7HCH4LJMH8W"
+  description = "description"
+}
